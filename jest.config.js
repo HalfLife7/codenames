79966bff76f49bc4d34d6544d41fd8837bc1cf6c
HalfLife7 <nguyendavid3322@gmail.com>
2022-02-03 00:38:38 -0500

    setup vite-jest

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: 'vite-jest',
+
+    testMatch: [
+        // '**/tests/unit/**/*.spec.ts?(x)',
+        // '**/__tests__/*.ts?(x)',
+        '**/tests/Frontend/**/*.ts?(x)'
+    ],
+    testEnvironment: 'jest-environment-jsdom'
+}
