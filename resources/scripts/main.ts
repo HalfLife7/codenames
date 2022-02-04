1edd9adbb8c64c653b0b4fea029eeaad47dc202b
HalfLife7 <nguyendavid3322@gmail.com>
2022-02-03 19:32:39 -0500

    add tailwind

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -3,13 +3,14 @@
 | Main entry point
 |--------------------------------------------------------------------------
 | Files in the "resources/scripts" directory are considered entrypoints
-| by default. 
-| 
+| by default.
+|
 | -> https://vitejs.dev
 | -> https://github.com/innocenzi/laravel-vite
 */
 
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from '@/views/App.vue'
+import '../css/index.css'
 
 createApp(App).mount('#app')
