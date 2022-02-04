01094b187959861381409cb7d87f197e7c81e207
HalfLife7 <nguyendavid3322@gmail.com>
2022-02-04 12:01:31 -0500

    create backend endpoint to get all cards

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
     testMatch: [
         // '**/tests/unit/**/*.spec.ts?(x)',
         // '**/__tests__/*.ts?(x)',
-        '**/tests/Frontend/**/*.ts?(x)'
+        '**/tests/Frontend/**/*.ts?(x)',
     ],
     testEnvironment: 'jest-environment-jsdom'
 }
--- tests/Frontend/Views/HelloWorld.spec.ts
+++ tests/Frontend/Views/HelloWorld.spec.ts
@@ -6,7 +6,6 @@ describe('HelloWorld', () => {
     it('should display header text!!', () => {
         const msg = 'Hello Vue 3'
         const wrapper = mount(HelloWorld, {props: {msg}})
-
         expect(wrapper.find('h1').text()).toEqual(msg)
     })
 })
