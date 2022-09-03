7c05c5fb5c914d9461841e238c6b98ba2dd3b0fa
HalfLife7 <nguyendavid3322@gmail.com>
2022-09-03 01:38:08 -0400

    upgrade to laravel 9

--- resources/scripts/main.ts
+++ resources/scripts/main.ts
@@ -18,7 +18,7 @@ import Echo from "laravel-echo";
 window.Pusher = Pusher
 window.Echo = new Echo({
     broadcaster: 'pusher',
-    key: import.meta.env.VITE_PUSHER_APP_ID,
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true
 });
